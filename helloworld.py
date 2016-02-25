112c8435c9575631eaba471a757e8b5d3cc54eab
Joseph Gonzalez <jgonzalez259@mail.valenciacollege.edu>
2016-02-24 23:51:05 -0500

    Chapter two greeting program

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-# replace the contents of this comment with your full name
+# Joseph Gonzalez
 
 # write a program that:
 # 1. greets the user in English
@@ -8,3 +8,17 @@
 
 # make sure that your code contains comments explaining your logic!
 
+
+
+print ('Hello class!')               #Greeting in english
+print ('A)French')                   #language choice 1
+print ('B)Italian')                  #language choice 2
+print ('C)Japanese')                 #language choice 3
+question1=input('Type the letter before the language you want to be greeted in!') #giving the user instruction to choose their desired language
+if question1 == "A" or question1 == "a":              #making sure both upper and lowercase Letter choices will work
+     print('bonjour')
+if question1 == "B" or question1 == "b":
+     print ('Ciao')
+if question1 == "C" or question1 == "c":
+     print ('Konnichiwa')
+exit
